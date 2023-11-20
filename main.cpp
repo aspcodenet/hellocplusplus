e081cbf491cb3f60b0a07275a78034c1e89878f1
aspcodenet <stefan.holmberg@systementor.se>
2023-11-20 08:50:27 +0100

    aa

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 #include <string>
 
+//Vad är jobbigt i C
+// input/output
+// strings
+// pekare
+// allokeringar
+// NYTT:
+// loopar for each
+
 int main(){
-    
+
     return 0;
 }
